869e38b4e01ae887a9bbd164b0593600a9202fb7
HubCreator <chocomilk8604@gmail.com>
2022-11-30 12:19:47 +0900

    feat(GenerateRandomNumberUtil) : 난수 번호 생성 util 클래스 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,19 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.util.GenerateRandomNumberUtil;
 import lotto.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        InputView.readPurchaseAmount();
+        int amount = InputView.readPurchaseAmount();
+        List<Lotto> lottos = new ArrayList<>();
+        for (int index = 0; index < amount; index++) {
+            lottos.add(GenerateRandomNumberUtil.generateLotto());
+        }
+
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.util.ValidationUtil;
 
 public class InputView {
-    public static void readPurchaseAmount() {
+    public static int readPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        ValidationUtil.isValidAmount(input);
+        int amount = ValidationUtil.isValidAmount(input);
+        System.out.println(String.format("%d개를 구매했습니다.", amount));
+        return amount;
     }
 }
