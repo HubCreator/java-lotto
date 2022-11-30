883b90a50b61f0f76ede920cabcf8c5561993884
HubCreator <chocomilk8604@gmail.com>
2022-11-30 14:03:08 +0900

    refactor(OutputView) : 출력 메서드 통일

--- src/main/java/lotto/enums/ViewMessage.java
+++ src/main/java/lotto/enums/ViewMessage.java
@@ -3,7 +3,8 @@ package lotto.enums;
 public enum ViewMessage {
     INPUT_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
     INPUT_WIN_LOTTO_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    OUTPUT_PURCHASE_COUNT("{0}개를 구매했습니다.");
 
     private final String value;
 
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,17 +3,33 @@ package lotto.view;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Result;
+import lotto.enums.ViewMessage;
+
+import java.text.MessageFormat;
 
 public class OutputView {
     public static void printPurchaseAmount(Lottos lottos) {
-        System.out.println(String.format("%d개를 구매했습니다.", lottos.size()));
+        print(MessageFormat.format(ViewMessage.OUTPUT_PURCHASE_COUNT.getValue(), lottos.size()));
+
         for (Lotto lotto : lottos) {
             lotto.sortNumbers();
-            System.out.println(lotto);
+            print(lotto);
         }
     }
 
     public static void printResult(Result result) {
+        print(result);
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
+    }
+
+    private static void print(Lotto lotto) {
+        System.out.println(lotto);
+    }
+
+    private static void print(Result result) {
         System.out.println(result);
     }
 }
