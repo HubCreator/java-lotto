fcc693e291663a26fcab7ba6e4cfa1d474a98e74
HubCreator <chocomilk8604@gmail.com>
2022-11-30 12:32:42 +0900

    refactor(Lottos) : static의 create 메서드 생성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,27 +1,18 @@
 package lotto;
 
-import lotto.domain.Lotto;
 import lotto.domain.Lottos;
-import lotto.util.GenerateRandomNumberUtil;
 import lotto.view.InputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int amount = InputView.readPurchaseAmount();
-        Lottos lottos = getLottos(amount);
+        Lottos lottos = Lottos.create(amount);
         List<Integer> winNumbers = InputView.readWinNumbers();
         int bonusNumber = InputView.readWinBonusNumber(winNumbers);
 
     }
 
-    private static Lottos getLottos(int amount) {
-        List<Lotto> lottos = new ArrayList<>();
-        for (int index = 0; index < amount; index++) {
-            lottos.add(GenerateRandomNumberUtil.generateLotto());
-        }
-        return new Lottos(lottos);
-    }
+
 }
--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.util.GenerateRandomNumberUtil;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lottos {
@@ -7,5 +10,13 @@ public class Lottos {
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public static Lottos create(int amount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int index = 0; index < amount; index++) {
+            lottos.add(GenerateRandomNumberUtil.generateLotto());
+        }
+        return new Lottos(lottos);
     }
 }
